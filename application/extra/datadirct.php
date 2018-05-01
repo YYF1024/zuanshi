2fcd89fb69ab0770f8b562557c9d22e788171775
root <panling_man@163.com>
2018-05-01 12:45:37 +0800

    data config

---
+++ application/extra/datadirct.php
@@ -0,0 +1,36 @@
+<?php
+//颜色
+return [
+'color'=>array( "1"=>'D',"2"=>'E',"3"=>'F',"4"=>'G', "5"=>'H',"6"=>'I',"7"=>'J',"8"=>'K',"9"=>'L',"10"=>'M',"11"=>'N',"12"=>'O',"13"=>'P',"14"=>'Q',"15"=>'R',"16"=>'S',"17"=>'T',"18"=>'U',"19"=>'V',"20"=>'W',"21"=>'X',"22"=>'Y',"23"=>'Z'),
+
+
+
+//净度
+'clarity'=> array( "1"=>'FL',"2"=>'IF',"3"=>'VVS1',"4"=>'VVS2',"5"=>'VS1',"6"=>'VS2',"7"=>'SI1',"8"=>'SI2',"9"=>'SI3',"10"=>'I1',"11"=>'I2',"12"=>'I3'),
+ 
+//切口 抛光 对称
+'Cut_symmetry_Polish'=> array("1"=>'Ideal',"2"=>'Excellent',"3"=>'Very Good',"4"=>'Good',"5"=>'Fair',"6"=>'Poor',"7"=>'None'),
+
+
+//fluorescence  荧光强度
+ 
+'FluorescenceIntensity'=>array("1"=>'None',"2"=>'Very Slight',"3"=>'Faint',"7"=>'Slight' ,"4"=>'Medium',"5"=>'Strong', "6"=>'Very Strong'),
+
+//fluorescence  荧光 颜色
+'FluorescenceColor' =>array("1"=>'Blue',"2"=>'Yellow',"3"=>'Green',"4"=>'Red',"5"=>'Orange',"6"=>'White'),
+
+
+//腰围 获得数据  以 -  分割
+'Girdle'=> array("1"=>'Extr. Thin',"2"=>'Very Thin',"3"=>'Thin',"4"=>'Slightly Thin',"5"=>'Medium',"6"=>'Slightly Thick',"7"=>'Thick',"8"=>'Very Thick',"9"=>'Extr. Thick'),
+
+//底面条件
+'CuletCondition'=> array("1"=>'Pointed',"2"=>'Chipped',"3"=>'Abraded'),
+//底面尺寸
+'CuletSize'=> array("1"=>'None',"5"=>'Very Small',"4"=>'Small',"3"=>'Medium',"2"=>'Large'),
+
+//Irradiated
+'Treatments'=> array('4'=>'Irradiated','1'=>'Laser Drilled','5'=>'HPHT','2'=>'Clarity Enhanced','3'=>'Color Enhanced','6'=>'Other'),
+
+//品牌
+'Brand' =>array("8"=>"Hearts and Arrows","1"=>"88-Cut","2"=>"Arctic Fox","10"=>"Argyle","3"=>"CanadaMark","11"=>"Canadian Ice","5"=>"Heart's On Fire","6"=>"Polar Bear","7"=>'Polar Ice')
+];
