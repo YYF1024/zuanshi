24bace5143d72288a9221d4ffd2e3d8433d1b25f
root <panling_man@163.com>
2018-05-10 15:45:31 +0800

    config

---
+++ wstmart/common/conf/extra/datadirct.php
@@ -0,0 +1,44 @@
+<?php
+ 
+return [
+//白钻颜色
+'color'=>array( "1"=>'D',"2"=>'E',"3"=>'F',"4"=>'G', "5"=>'H',"6"=>'I',"7"=>'J',"8"=>'K',"9"=>'L',"10"=>'M',"11"=>'N',"12"=>'O',"13"=>'P',"14"=>'Q',"15"=>'R',"16"=>'S',"17"=>'T',"18"=>'U',"19"=>'V',"20"=>'W',"21"=>'X',"22"=>'Y',"23"=>'Z'),
+
+//彩钻颜色
+'Fancycolor'=>array( "1"=>'Yellow',"2"=>'Pink',"3"=>'Blue',"4"=>'Red', "5"=>'Green',"6"=>'Purple',"7"=>'Orange',"8"=>'Violet',"9"=>'Gray',"10"=>'Black',"11"=>'Brown',"12"=>'Champagne',"13"=>'Cognac',"14"=>'Other',"15"=>'Chameleon',"16"=>'White' ),
+ 
+ //彩钻颜色强度
+"Intensity" =>array("1"=>'Faint',"2"=>'Very Light',"3"=>'Light',"4"=>'Fancy Light',"5"=>'Fancy',"6"=>'Fancy Dark',"7"=>'Fancy Intense',"8"=>'Fancy Vivid',"9"=>'Fancy Deep' ),
+
+
+//净度
+'clarity'=> array( "1"=>'FL',"2"=>'IF',"3"=>'VVS1',"4"=>'VVS2',"5"=>'VS1',"6"=>'VS2',"7"=>'SI1',"8"=>'SI2',"9"=>'SI3',"10"=>'I1',"11"=>'I2',"12"=>'I3'),
+ 
+//切口 抛光 对称
+'Cut_symmetry_Polish'=> array("1"=>'Ideal',"2"=>'Excellent',"3"=>'Very Good',"4"=>'Good',"5"=>'Fair',"6"=>'Poor',"7"=>'None'),
+
+
+//fluorescence  荧光强度
+ 
+'FluorescenceIntensity'=>array("1"=>'None',"2"=>'Very Slight',"3"=>'Faint',"7"=>'Slight' ,"4"=>'Medium',"5"=>'Strong', "6"=>'Very Strong'),
+
+//fluorescence  荧光 颜色
+'FluorescenceColor' =>array("1"=>'Blue',"2"=>'Yellow',"3"=>'Green',"4"=>'Red',"5"=>'Orange',"6"=>'White'),
+
+
+//腰围 获得数据  以 -  分割
+'Girdle'=> array("1"=>'Extr. Thin',"2"=>'Very Thin',"3"=>'Thin',"4"=>'Slightly Thin',"5"=>'Medium',"6"=>'Slightly Thick',"7"=>'Thick',"8"=>'Very Thick',"9"=>'Extr. Thick'),
+
+//底面条件
+'CuletCondition'=> array("1"=>'Pointed',"2"=>'Chipped',"3"=>'Abraded'),
+//底面尺寸
+'CuletSize'=> array("1"=>'None',"5"=>'Very Small',"4"=>'Small',"3"=>'Medium',"2"=>'Large'),
+
+//处理
+'Treatments'=> array('4'=>'Irradiated','1'=>'Laser Drilled','5'=>'HPHT','2'=>'Clarity Enhanced','3'=>'Color Enhanced','6'=>'Other'),
+
+//品牌
+// 'Brand' =>array("8"=>"Hearts and Arrows","1"=>"88-Cut","2"=>"Arctic Fox","10"=>"Argyle","3"=>"CanadaMark","11"=>"Canadian Ice","5"=>"Heart's On Fire","6"=>"Polar Bear","7"=>'Polar Ice')
+
+ 
+];
